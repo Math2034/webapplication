f82b7b1ea217953d7498f42317b65b23c0263b4c
Matheus R <61693870+Math2034@users.noreply.github.com>
2023-07-09 16:59:16 -0300

    Add files via upload

---
+++ Models/ContextoMongodb.cs
@@ -0,0 +1,46 @@
+﻿using MongoDB.Driver;
+
+namespace WebApplication2.Models
+{
+    public class ContextoMongodb
+    {
+        public static string? ConnectionString { get; set; }
+
+        public static string? Database { get; set; }
+
+        public static bool IsSSL { get; set; }
+        private IMongoDatabase _database { get; }
+
+        public ContextoMongodb()
+        {
+            try
+            {
+                MongoClientSettings setting = MongoClientSettings.FromUrl(new MongoUrl(ConnectionString));
+
+                if(IsSSL)
+                {
+                    setting.SslSettings = new SslSettings
+                    {
+                        EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12
+                    };
+                }
+                var mongoCliente = new MongoClient(setting);
+                _database = mongoCliente.GetDatabase(Database);
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("Não deu pra conectar :(");
+            }
+        }
+
+        public IMongoCollection<Usuario> Usuario
+        {
+            get
+            {
+                return _database.GetCollection<Usuario>("Usuario");
+            }
+        }
+
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication2.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+
+        [Column("Id")]
+        [Display(Name = "ID")]
+        public Guid Id { get; set; }
+
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        [RegularExpression(@"^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ'-'\s]{1,40}$", ErrorMessage = "Por favor, digite um nome válido.")]
+        [Required(ErrorMessage = "O campo 'Nome Completo' é obrigatório.")]
+        public String? Nome { get; set; }
+
+
+        [Column("Email")]
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "O campo 'Email' é obrigatório.")]
+        [RegularExpression("^[a-z0-9_\\+-]+(\\.[a-z0-9_\\+-]+)*@[a-z0-9-]+(\\.[a-z0-9]+)*\\.([a-z]{2,4})$", ErrorMessage = "Por favor, digite um Email válido.")]
+        [EmailAddress(ErrorMessage = "Por favor, digite um nome válido.")]
+        public String? Email { get; set; }
+
+
+        [Column("Data de Nascimento")]
+        [Display(Name = "Data_Nascimento")]
+        [Required(ErrorMessage = "O campo 'Data de nascimento' é obrigatório.")]
+        [DataType(DataType.Date)]
+        public DateTime Data_Nascimento { get; set; }
+
+
+        [Column("Rendimento")]
+        [Display(Name = "Rendimento")]
+        [Range(0, double.MaxValue, ErrorMessage = "Por favor, digite um valor acima de -1.")]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:c}", ApplyFormatInEditMode = true)]
+        public decimal Rendimento { get; set; }
+    }
+}
+
